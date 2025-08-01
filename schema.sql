3360cac33e5b349dd22e7471bf08d1c9049533fc
Nea Karenius <nea.karenius@gmail.com>
2025-08-01 11:02:43 +0300

    fixed a double reference between tables

---
+++ schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    password_hash TEXT
+);
+
+CREATE TABLE pets (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    species TEXT,
+    breed TEXT,
+    user_id INTEGER REFERENCES users
+);
+
+
+CREATE TABLE characteristics (
+    id INTEGER PRIMARY KEY,
+    characteristic TEXT
+);
+
+CREATE TABLE pet_characteristics (
+    pet_id INTEGER REFERENCES pets,
+    characteristic_id REFERENCES characteristics
+);
+
+
+CREATE TABLE messages (
+    id INTEGER PRIMARY KEY,
+    content TEXT,
+    sent_at TEXT,
+    user_id INTEGER REFERENCES users,
+    pet_id INTEGER REFERENCES pets
+);
